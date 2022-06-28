933f34afec9c9591e32167ba22b9003763f983cc
Jaron Hadley <jaron.hadley@gmail.com>
2022-06-28 11:37:19 -0600

    updated so when hour elapses the formatting updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,22 +10,21 @@ var two = $("#2p");
 var three = $("#3p");
 var four = $("#4p");
 var five = $("#5p");
-// time block link
-var timeBlock = $(".time-block");
 // save button link
 var saveBtn = $(".saveBtn");
 
-
 // report today's date
-currentDay.textContent = moment().format("dddd, MMM Do YYYY");
+currentDay.textContent = moment().format("dddd, MMMM Do YYYY");
 
 // time block array
 var times = [nine,ten,eleven, twelve,one,two,three,four,five];
-// add past present future classes to timeslots
-// update once hour elapses
+
+// add past present future classes to timeslots for formatting
 function renderBlocks (){
     currentHour = moment().hour();
     times.forEach(hour =>{
+        hour.removeClass("present");
+        hour.removeClass("future");
         if(hour.data("hour")<currentHour){
             hour.addClass("past");
         } else if(hour.data("hour")==currentHour){
@@ -42,6 +41,7 @@ function renderEvents (){
         hour.text(localStorage.getItem(hour.data("storagename")));
     })
 }
+
 // save new events to local storage variables
 function saveNewEvents (event) {
     var description = $("#"+$(event.target).data("storagename"));
@@ -49,7 +49,7 @@ function saveNewEvents (event) {
     renderEvents(); // render the events again with updates
 }
 
-renderBlocks();
-renderEvents();
-setInterval(renderBlocks,60000);
-saveBtn.on("click",saveNewEvents);
+renderBlocks(); // add past, present and future formatting to blocks
+renderEvents(); // add stored events to blocks
+setInterval(renderBlocks,60000); // check to see if hour has elapsed
+saveBtn.on("click",saveNewEvents); // check for new saves on events
